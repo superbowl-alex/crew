f4345772a45b68cd8f7e6bad08fb5642888b193a
Oleksandr <71338915+superbowl-alex@users.noreply.github.com>
2022-05-30 19:51:14 +0300

    Adds modal/about

---
+++ js/modal-about.js
@@ -0,0 +1,16 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    backdrop: document.querySelector('[data-about-backdrop]'),
+    modal: document.querySelector('[data-about-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    document.body.classList.toggle('modal-open');
+    refs.backdrop.classList.toggle('backdrop--hidden');
+    refs.modal.classList.toggle('modal-is-open');
+    refs.openModalBtn.classList.toggle('open');
+  }
+})();
