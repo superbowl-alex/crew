f7dae1bbffe22c60782422a9edb9ecc255c93a9d
Oleksandr <71338915+superbowl-alex@users.noreply.github.com>
2022-05-31 09:11:32 +0300

    Update modal-about.js

--- js/modal-about.js
+++ js/modal-about.js
@@ -16,23 +16,25 @@
 //     refs.openModalBtn.classList.toggle('open');
 //   }
 // })();
-const buttonEL = document.querySelector(".about__btn");
-const headerEL = document.querySelector("header");
-const backdropEL = document.querySelector(".backdrop");
-const modalEL = document.querySelector("div[data-about-modal]")
+const buttonEL = document.querySelector('.about__btn');
+const headerEL = document.querySelector('header');
+const backdropEL = document.querySelector('.backdrop');
+const modalEL = document.querySelector('div[data-about-modal]');
+const bodylEL = document.querySelector('body');
 
-buttonEL.addEventListener("click", openModal)
+buttonEL.addEventListener('click', openModal);
 
 function openModal() {
-  headerEL.classList.toggle("hidden-header");
-  backdropEL.classList.toggle("backdrop--hidden");
+  headerEL.classList.toggle('hidden-header');
+  backdropEL.classList.toggle('backdrop--hidden');
 
-  modalEL.classList.toggle("transform");
-  modalEL.classList.toggle("about__modal");
+  modalEL.classList.toggle('transform');
+  modalEL.classList.toggle('about__modal');
+  bodylEL.classList.toggle('modal-open');
 
-  if (modalEL.classList.contains("transform")) {
-    buttonEL.firstElementChild.textContent = "Close";
+  if (modalEL.classList.contains('transform')) {
+    buttonEL.firstElementChild.textContent = 'Close';
   } else {
-    buttonEL.firstElementChild.textContent = "Read more";
+    buttonEL.firstElementChild.textContent = 'Read more';
   }
 }
