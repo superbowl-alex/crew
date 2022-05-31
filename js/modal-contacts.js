a66d376a339f41e2c865e0f0f85aa43d422809e1
Oleksandr <71338915+superbowl-alex@users.noreply.github.com>
2022-05-31 17:45:51 +0300

    Fixed contacts modal

--- js/modal-contacts.js
+++ js/modal-contacts.js
@@ -1,36 +1,20 @@
-const buttonOpenEL = document.querySelector('.contacts-btn-2');
-const buttonCloseEL = document.querySelector('.contacts-modal__btn-close');
-const headerEL = document.querySelector('header');
-const backdropEL = document.querySelector('.contacts-backdrop');
-const modalEL = document.querySelector('div[data-contacts-modal]');
-const bodylEL = document.querySelector('body');
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('.contacts-btn-2'),
+    closeModalBtn: document.querySelector('.contacts-modal__btn-close'),
+    backdrop: document.querySelector('.contacts-backdrop'),
+    modal: document.querySelector('.contacts-modal'),
+    header: document.querySelector('header'),
+    body: document.querySelector('.body'),
+  };
 
-// buttonEL.addEventListener('click', openModal);
-buttonOpenEL.addEventListener('click', openModal);
-buttonCloseEL.addEventListener('click', closeModal);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
-function openModal() {
-  headerEL.classList.toggle('hidden-header');
-  backdropEL.classList.toggle('backdrop--hidden');
-
-  modalEL.classList.toggle('transform');
-  modalEL.classList.toggle('contacts-modal');
-  bodylEL.classList.toggle('modal-open');
-}
-function closeModal() {}
-
-// (() => {
-//   const refs = {
-//     openModalBtn: document.querySelector('[data-modal-open]'),
-//     closeModalBtn: document.querySelector('[data-modal-close]'),
-//     modal: document.querySelector('[data-modal]'),
-//   };
-
-//   refs.openModalBtn.addEventListener('click', toggleModal);
-//   refs.closeModalBtn.addEventListener('click', toggleModal);
-
-//   function toggleModal() {
-//     document.body.classList.toggle('modal-open');
-//     refs.modal.classList.toggle('backdrop--hidden');
-//   }
-// })();
+  function toggleModal() {
+    refs.backdrop.classList.toggle('backdrop--hidden');
+    refs.modal.classList.toggle('contacts-modal-open');
+    refs.header.classList.toggle('hidden-header');
+    refs.body.classList.toggle('modal-open');
+  }
+})();
