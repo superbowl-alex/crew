ec4a6ddebff90475419af8c547f41f52d4d8ed90
Oleksandr <superbowl@ukr.net>
2022-09-19 23:17:25 +0300

    Bugfix scroll to

---
+++ js/scroll-to-archor.js
@@ -0,0 +1,23 @@
+const menuLinks = document.querySelectorAll(
+  '.header__nav-link[data-goto], .mobile-menu__link[data-goto]',
+);
+if (menuLinks.length > 0) {
+  menuLinks.forEach(menuLink => {
+    menuLink.addEventListener('click', onMenuLinkClick);
+  });
+  function onMenuLinkClick(e) {
+    const menuLink = e.target;
+    if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+      const goToBlock = document.querySelector(menuLink.dataset.goto);
+      const goToBlockValue =
+        goToBlock.getBoundingClientRect().top +
+        pageYOffset -
+        document.querySelector('header').offsetHeight;
+      window.scrollTo({
+        top: goToBlockValue,
+        behavior: 'smooth',
+      });
+      e.preventDefault();
+    }
+  }
+}
